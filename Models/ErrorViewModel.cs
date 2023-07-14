d9fc8dff6d25aa0636a2788ae92fe337c7acf9fb
Cairo Braz Costa <cairo.costa@animaeducacao.com.br>
2023-07-14 08:44:55 -0300

    Criando projeto mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Aula_Scaffold.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
